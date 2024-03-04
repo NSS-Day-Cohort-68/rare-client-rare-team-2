7f730fbad9da66462612684e10a6c3de24f86e9b
Jean Patimeteeporn <jpatimet@gmail.com>
2024-03-04 15:56:20 -0600

    added starter code

--- rare-team-2/src/App.js
+++ rare-team-2/src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Login } from "./components/auth/Login.js";
+import { Register } from "./components/auth/Register.js";
+import { Authorized } from "./views/Authorized.js";
+import { ApplicationViews } from "./views/ApplicationView.js";
 
 function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-  return {
-   
-  }
+      <Route
+        path="*"
+        element={
+          <Authorized>
+            <ApplicationViews />
+          </Authorized>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
