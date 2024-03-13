c1dcc2d5afeeb7f304f4fadbd6aa70e8f3e1931b
Jean Patimeteeporn <jpatimet@gmail.com>
2024-03-13 14:53:21 -0500

    added button to post details to  navigate user to comment list page

--- rare-team-2/src/components/Posts/PostDetails.js
+++ rare-team-2/src/components/Posts/PostDetails.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { getPostdets } from "../../services/postServices";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export const PostDetails = () => {
   const [post, setPost] = useState({});
-  const {postId} = useParams()
+  const { postId } = useParams();
 
   useEffect(() => {
     fetchPostdets();
-    console.log(post.author_username)
+    console.log(post.author_username);
   }, []);
 
   const fetchPostdets = () => {
@@ -20,21 +20,24 @@ export const PostDetails = () => {
 
   return (
     <ul>
-        <li
-          key={post.id}
-          data-type="post"
-          data-id={post.id}
-          data-title={post.title}
-          data-content={post.content}
-          className="publicationDate"
-        >
-          {post.publicationDate}
-        </li>
-        <h2>{post.title}</h2>
-        <ol>{post.content}</ol>
-        <ol>{post.publication_date}</ol>
-        
-        <ol>{post.author_username}</ol>
+      <li
+        key={post.id}
+        data-type="post"
+        data-id={post.id}
+        data-title={post.title}
+        data-content={post.content}
+        className="publicationDate"
+      >
+        {post.publicationDate}
+      </li>
+      <h2>{post.title}</h2>
+      <ol>{post.content}</ol>
+      <ol>{post.publication_date}</ol>
+
+      <ol>{post.author_username}</ol>
+      <Link to={`/allposts/${post?.id}/comments`}>
+        <button>View Comments</button>
+      </Link>
     </ul>
   );
 };
